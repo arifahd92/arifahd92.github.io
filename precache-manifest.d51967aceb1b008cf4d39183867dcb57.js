9f8ac2291c82dc19475ce9eac9f1f1c4246963b9
unknown <arifahd92@gmail.com>
2022-12-08 17:07:03 +0530

    Updates

---
+++ precache-manifest.d51967aceb1b008cf4d39183867dcb57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab64e07624a31776c2db",
+    "url": "/react-upload/static/css/main.7a7c6575.chunk.css"
+  },
+  {
+    "revision": "ab64e07624a31776c2db",
+    "url": "/react-upload/static/js/main.ab64e076.chunk.js"
+  },
+  {
+    "revision": "b68f4732025e137395b3",
+    "url": "/react-upload/static/js/1.b68f4732.chunk.js"
+  },
+  {
+    "revision": "ca57e88ecb0536cdd540",
+    "url": "/react-upload/static/js/runtime~main.ca57e88e.js"
+  },
+  {
+    "revision": "afd46c58373c59a3c3aae28700bc6c08",
+    "url": "/react-upload/index.html"
+  }
+];
